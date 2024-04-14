77e32dc9f7683e77aeeddfa785c9698a7a1cd744
Lee Roy Akbar <leeroyakbar@gmail.com>
2024-04-14 08:25:44 +0700

    learn-bufio-read-write

---
+++ bufioRead.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strings"
+)
+
+func main() {
+	input := strings.NewReader("string ahoyy\nini newline \n")
+	reader := bufio.NewReader(input)
+
+	for {
+		line, _, err := reader.ReadLine()
+		if err == io.EOF {
+			break
+		}
+		fmt.Println(string(line))
+	}
+}
